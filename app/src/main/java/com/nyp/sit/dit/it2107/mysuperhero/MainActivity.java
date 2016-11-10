052ddeb4c40daecd83a2b3931441890744d4bcc0
152772a <152772a@mymail.nyp.edu.sg>
2016-11-10 14:06:33 +0800

    int

--- app/src/main/java/com/nyp/sit/dit/it2107/mysuperhero/MainActivity.java
+++ app/src/main/java/com/nyp/sit/dit/it2107/mysuperhero/MainActivity.java
@@ -1,14 +1,18 @@
 package com.nyp.sit.dit.it2107.mysuperhero;
 
+import android.content.res.Resources;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Spinner spLocations;
+   ListView spLocations;
     ArrayAdapter<CharSequence> adapter;
     TextView superHeroTV;
     String[] strArray;
@@ -24,7 +28,43 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         superHeroTV = (TextView) findViewById(R.id.superHerosTV);
-        spLocations = (Spinner) findViewById(R.id.spLocations);
+        spLocations = (ListView) findViewById(R.id.spLocations);
+
+
+        Resources myRes = this.getResources();
+        strArray = myRes.getStringArray(R.array.locationArea);
+
+        adapter= ArrayAdapter.createFromResource(this,R.array.locationArea, android.
+        R.layout.simple_spinner_dropdown_item);
+        spLocations.setAdapter(adapter);
+
+//        spLocations.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                String text = superHeros[position];
+//                superHeroTV.setText(text);
+//            }
+//
+//            @Override
+//            public void onNothingSelected(AdapterView<?> parent) {
+//
+//                superHeroTV.setText("There is no super Heroes for this area. ");
+//
+//            }
+//        });
+
+
+        spLocations.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String text = superHeros[position];
+                superHeroTV.setText(text);
+
+
+            }
+        });
+
+
 
     }
 }
